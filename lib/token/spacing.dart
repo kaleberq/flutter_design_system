0aaeab9a5b595f10eeb056bc1ba2e52b6201d9b6
kalebe <kalebemisael@gmail.com>
2026-01-07 09:25:08 -0300

    Remove spacing documentation comments from the Spacing class in spacing.dart.

--- lib/token/spacing.dart
+++ lib/token/spacing.dart
@@ -5,24 +5,11 @@
 class Spacing {
   Spacing._();
 
-  /// Espaçamento extra pequeno: 4.0
   static const double xs = 4.0;
-
-  /// Espaçamento pequeno: 8.0
   static const double sm = 8.0;
-
-  /// Espaçamento médio: 16.0
   static const double md = 16.0;
-
-  /// Espaçamento grande: 24.0
   static const double lg = 24.0;
-
-  /// Espaçamento extra grande: 32.0
   static const double xl = 32.0;
-
-  /// Espaçamento extra extra grande: 48.0
   static const double xxl = 48.0;
-
-  /// Espaçamento extra extra extra grande: 64.0
   static const double xxxl = 64.0;
 }
