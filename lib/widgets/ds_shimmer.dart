1b2dcc125b80afe2ca8f46bb75f6364fdb881c7b
kaleberq <kalebemisael@gmail.com>
2026-08-27 13:23:01 -0300

    fix: correct class name to DSShimmer and update container color variable name

--- lib/widgets/ds_shimmer.dart
+++ lib/widgets/ds_shimmer.dart
@@ -3,45 +3,45 @@ import 'package:flutter_design_system/tokens/ds_colors.dart';
 import 'package:flutter_design_system/tokens/ds_radius.dart';
 import 'package:shimmer/shimmer.dart';
 
-class DsShimmer extends StatelessWidget {
+class DSShimmer extends StatelessWidget {
   final double? width;
   final double? height;
   final double radius;
   final Color baseColor;
   final Color highlightColor;
-  final Color containerColor;
+  final Color contrainerColor;
 
-  const DsShimmer.rectangular({
+  const DSShimmer.rectangular({
     super.key,
     this.width,
     this.height,
     this.radius = DSRadius.md,
-    this.containerColor = DSColors.white,
+    this.contrainerColor = DSColors.white,
     this.baseColor = DSColors.surfaceLight,
     this.highlightColor = DSColors.lightGrey,
   });
 
-  const DsShimmer.circular({
+  const DSShimmer.circular({
     super.key,
     this.width,
     this.height,
     this.radius = DSRadius.circular,
-    this.containerColor = DSColors.white,
+    this.contrainerColor = DSColors.white,
     this.baseColor = DSColors.surfaceLight,
     this.highlightColor = DSColors.lightGrey,
   });
 
   @override
   Widget build(BuildContext context) => Shimmer.fromColors(
-        baseColor: baseColor,
-        highlightColor: highlightColor,
-        child: Container(
-          width: width,
-          height: height,
-          decoration: BoxDecoration(
-            color: containerColor,
-            borderRadius: BorderRadius.circular(radius),
-          ),
-        ),
-      );
+    baseColor: baseColor,
+    highlightColor: highlightColor,
+    child: Container(
+      height: height,
+      constraints: BoxConstraints(maxWidth: width ?? 0,),
+      decoration: BoxDecoration(
+        color: contrainerColor,
+        borderRadius: BorderRadius.circular(radius),
+      ),
+    ),
+  );
 }
